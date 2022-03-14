eee9d8e60229509b9578f9fcd0a10966eee3b8c8
Timo <developer.timo@gmail.com>
2022-03-14 11:15:33 +0900

    [프록시 패턴과 데코레이터 패턴] 예제 프로젝트 만들기 v1
    - 설정 클래스 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,11 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AppV1Config.class) //클래스를 스프링 빈으로 등록할 때 사용
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
