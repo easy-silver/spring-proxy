9be638f6aaf841b68e202918fbd66a7b0b649de0
Timo <developer.timo@gmail.com>
2022-03-15 23:58:33 +0900

    [프록시 패턴과 데코레이터 패턴] 예제 프로젝트 만들기 v2
    - 구현체만 가진 컨트롤러, 서비스, 레포지토리 생성

---
+++ src/main/java/hello/proxy/app/v2/OrderControllerV2.java
@@ -0,0 +1,29 @@
+package hello.proxy.app.v2;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Slf4j
+@RequestMapping //@Controller 어노테이션 사용 시 컴포넌트 스캔 대상이 되기 때문에 @RequestMapping 사용하여 컨트롤러임을 인식하게 설정함
+@ResponseBody
+public class OrderControllerV2 {
+
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
