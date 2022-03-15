f2e061b71ee6b22f03fa6c724a0351cf101b847f
Timo <developer.timo@gmail.com>
2022-03-16 00:01:03 +0900

    [프록시 패턴과 데코레이터 패턴] 예제 프로젝트 만들기 v2
    - V1, V2 설정 모두 사용하도록 처리

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-//@Import(AppV1Config.class) //클래스를 스프링 빈으로 등록할 때 사용
-@Import(AppV2Config.class)
+@Import({AppV1Config.class, AppV2Config.class}) //클래스를 스프링 빈으로 등록할 때 사용
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
